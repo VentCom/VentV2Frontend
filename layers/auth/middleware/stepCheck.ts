1f397e269f7f5670d8e3c3a0806fdad0ee55f54c
Sylvester Nkeze <snkeze9@gmail.com>
2025-08-13 10:39:06 +0100

    Update: Onboarding process completed

--- layers/auth/middleware/stepCheck.ts
+++ layers/auth/middleware/stepCheck.ts
@@ -1,9 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
   const onboardingStore = useOnboardingStore();
-  console.log(to.name as string);
   const onboardingRouteName = onboardingStore.getOnboardingRouteName(
     to.name as string
   );
-  console.log("onboardingRouteName", onboardingRouteName);
-  //   onboardingStore.redirectOnFalseActivity(onboardingRouteName);
 });
